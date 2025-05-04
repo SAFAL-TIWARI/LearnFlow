d2b5d6ef0b7590ca4f3ca61ae5b9e7fd54e5b490
SAFAL-TIWARI <safaltiwari602@gmail.com>
2025-05-04 16:13:28 +0530

    Integrate adsense and logo and errors

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: "./", // Use relative paths for assets
   plugins: [
     react(),
     nextAuthPlugin(),
