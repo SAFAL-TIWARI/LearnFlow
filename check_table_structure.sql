23c8d89048e897871719af72f3fd97052ef83c1f
SAFAL-TIWARI <safaltiwari602@gmail.com>
2025-06-15 20:30:36 +0530

    sql files removed

--- check_table_structure.sql
+++
@@ -1,5 +0,0 @@
--- This query will show the column names in your user_files table
-SELECT column_name, data_type 
-FROM information_schema.columns 
-WHERE table_schema = 'public' 
-AND table_name = 'user_files';
--- check_user_files_columns.sql
+++
@@ -1,6 +0,0 @@
--- Check the actual column names in the user_files table
-SELECT column_name 
-FROM information_schema.columns 
-WHERE table_schema = 'public' 
-AND table_name = 'user_files'
-ORDER BY ordinal_position;
